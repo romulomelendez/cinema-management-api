ff61c61e1f9fbdeaa598bfe1acb7145a968650d3
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-12-23 23:12:42 -0300

    cors dependency added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express, { json } from "express"
+import cors from "cors"
 
 const app = express()
 const PORT = 3000
 
-// app.use(cors())
+app.use(cors())
 app.use(json())
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
