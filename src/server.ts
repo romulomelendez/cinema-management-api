0e71170d43bf94c24070a356e0101b363dc50638
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-12-24 12:26:26 -0300

    app and server responsabilities separated

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
-import express, { json } from "express"
-import cors from "cors"
+import { app } from './app'
 
-const app = express()
 const PORT = 3000
-
-app.use(cors())
-app.use(json())
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
